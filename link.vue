db52277b755c2b721a54488bbcdfa9b11be90f1b
polminsk <polminsk@mail.ru>
2020-08-13 20:18:47 +0300

    my2020

---
+++ link.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="services">
+		<h1>{{title}}</h1>
+		<p>
+			Ссылки на сайты.
+			<br>
+			<br>
+			Очень важные ссылки.
+
+		</p>
+	</div>
+</template>
+<script>
+	export default{
+		name:'link',
+		data (){
+			return{
+				title:'Link'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
